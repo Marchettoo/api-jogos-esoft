1efdd0adf41a24bf029379dbff4dd9dff962559f
Marchettoo <gabirelmarchetto60@gmail.com>
2026-05-20 13:31:30 -0300

    cors

--- src/main/java/br/com/esoft/apijogos/ApiController.java
+++ src/main/java/br/com/esoft/apijogos/ApiController.java
@@ -8,7 +8,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-@CrossOrigin(origins = "*") // <-- ESTA É A LINHA QUE LIBERA O CORS PARA O EXPO SNACK
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class ApiController {
